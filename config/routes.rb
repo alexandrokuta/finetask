58effc8ffa4306ce304aad9ff1a5c6744aa707e1
alexandrokuta <alexandrokuta@ritsutsujitomohironoMacBook-ea.local>
2019-01-05 14:18:56 +0900

    edit this file

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,9 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
   end
-  resources :tasks
   root to: 'tasks#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-
-Rails.application.routes.draw do
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  namespace :admin do
-    resources :users
+  resources :tasks do
+    post :confirm, action: :confirm_new, on: :new
   end
-
-  root to: 'tasks#index'
-  resources :tasks
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
